3ba82716c87e43daf96cae749836ed60b8a6d959
Busato Gabriel <busatogabriel96@gmail.com>
2024-04-25 22:17:36 -0300

    v6.0

---
+++ src/components/Admin/Bookings/BookingProfile/BookingProfile.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./BookingProfile.css"
+
+export default function BookingProfile() {
+  return (
+    <div>BookingProfile</div>
+  )
+}
--- src/components/Admin/Bookings/BookingsAdmin.jsx
+++ src/components/Admin/Bookings/BookingsAdmin.jsx
@@ -1,47 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
+import "./BookingsAdmin.css";
 
 function BookingsAdmin() {
-  const [mascotas, setMascotas] = useState([]);
-  const [candidatos, setCandidatos] = useState([]);
+  const [data, setData] = useState([]);
   const [busqueda, setBusqueda] = useState('');
 
   useEffect(() => {
     fetch("https://adoptaapp.pythonanywhere.com/candidatos")
       .then(response => response.json())
-      .then(data => {
-        // Aplicar filtro si hay algo en el input de búsqueda
-        if (busqueda.trim() !== '') {
-          const candidatosFiltradas = filtrarCandidatos(data, busqueda);
-          setCandidatos(candidatosFiltradas);
-        } else {
-          setCandidatos(data);
-        }
+      .then(dataC => {
+        fetch("https://adoptaapp.pythonanywhere.com/mascotas")
+          .then(response => response.json())
+          .then(dataM => {
+            const combinedData = dataC.map(candidato => {
+              const mascota = dataM.find(mascota => mascota.id === candidato.id_mascota);
+              return { ...candidato, mascota: mascota };
+            });
+            setData(combinedData);
+          })
+          .catch(error => console.error("Error fetching mascotas:", error));
       })
-      .catch(error => console.error("Error fetching mascotas:", error));
-  }, [busqueda]); // Dependencia añadida para que se ejecute cada vez que cambie la búsqueda
-  useEffect(() => {
-    fetch("https://adoptaapp.pythonanywhere.com/mascotas")
-      .then(response => response.json())
-      .then(data => {
-        // Aplicar filtro si hay algo en el input de búsqueda
-        if (busqueda.trim() !== '') {
-          const mascotasFiltradas = filtrarCandidatos(data, busqueda);
-          setMascotas(mascotasFiltradas);
-        } else {
-          setMascotas(data);
-        }
-      })
-      .catch(error => console.error("Error fetching mascotas:", error));
-  }, [busqueda]); // Dependencia añadida para que se ejecute cada vez que cambie la búsqueda
+      .catch(error => console.error("Error fetching candidatos:", error));
+  }, []); // Solo se ejecuta una vez al montar el componente
 
-
-
-  // Función para filtrar mascotas según el criterio de búsqueda
-  const filtrarCandidatos = (candidatos, busqueda) => {
-    return candidatos.filter(candidato =>
+  // Función para filtrar candidatos según el criterio de búsqueda
+  const filtrarCandidatos = (data, busqueda) => {
+    return data.filter(candidato =>
       candidato.nombreApellido.toLowerCase().includes(busqueda.toLowerCase())
     );
+  };
+
+
+  const handleEliminarCandidato = (id) => {
+    fetch(`https://adoptaapp.pythonanywhere.com/candidatos/delete/${id}`, {
+      method: 'DELETE'
+    })
+    .then(response => {
+      if (response.ok) {
+        setData(prevData => prevData.filter(item => item.id !== id));
+        document.querySelector('p.alert.success').style.display = 'flex';
+      } else {
+        console.error("Error al eliminar el candidato");
+      }
+    })
+    .catch(error => console.error("Error al eliminar el candidato:", error));
   };
 
   return (
@@ -66,18 +69,17 @@ function BookingsAdmin() {
               <th>Nombre</th>
               <th>Email</th>
               <th>Info</th>
-           
-       
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {candidatos.map(candidato => (
-              <tr key={candidato.id_mascota}>
-                <td>Pepito</td>
+            {filtrarCandidatos(data, busqueda).map(candidato => (
+              <tr key={candidato.id}>
+                <td>{candidato.mascota.nombre}</td>
                 <td>{candidato.nombreApellido}</td>
                 <td>{candidato.email}</td>
-               
-                <td><a href={`/Admin/Bookings/${candidato.id_mascota}`} className="Edit">Ver más</a></td>
+                <td><Link to={`/Admin/Bookings/${candidato.id}`} className="MoreInfo">Ver más</Link></td>
+                <td><a onClick={() => handleEliminarCandidato(candidato.id)} className="DeleteX">X</a></td>
               </tr>
             ))}
           </tbody>
